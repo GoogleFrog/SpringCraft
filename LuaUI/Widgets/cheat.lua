713e129e7bfa74e681ec163767df868a8604d5ad
GoogleFrog <googl3frog@gmail.com>
2019-04-05 14:38:29 +1100

    Added restarting.

--- LuaUI/Widgets/cheat.lua
+++ LuaUI/Widgets/cheat.lua
@@ -43,5 +43,48 @@ function widget:KeyPress(key, modifier, isRepeat)
 		if key == KEYSYMS.Q then
 			Spring.SendCommands{"quitforce"}
 		end
+		if key == KEYSYMS.R then
+			local startScript = [[[game]
+{
+[allyteam1]
+{
+numallies=0;
+}
+[team1]
+{
+teamleader=1;
+allyteam=1;
+}
+[modoptions]
+{
+maxspeed=20;
+}
+[allyteam0]
+{
+numallies=0;
+}
+[team0]
+{
+teamleader=0;
+allyteam=0;
+}
+[player1]
+{
+team=1;
+name=Enemy;
+}
+[player0]
+{
+team=0;
+name=UnnamedPlayer;
+}
+mapname=Adansonia v4.1;
+myplayername=UnnamedPlayer;
+ishost=1;
+gametype=SpringCraft $VERSION;
+nohelperais=0;
+}]]
+			Spring.Restart("", startScript)
+		end
 	end
 end
