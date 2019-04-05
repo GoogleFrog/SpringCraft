ce7d1bb365140e130ccd1ee95ba052315fe2ce06
GoogleFrog <googl3frog@gmail.com>
2019-04-05 12:58:51 +1100

    Initial commit of an almost empty game based on Skeleton Game, based on Spring ABC.

---
+++ ModInfo.lua
@@ -0,0 +1,13 @@
+local modinfo = {
+	name = "SpringCraft",
+	shortname = "SC",
+	version = "$VERSION",
+	description = "Tech demo for relatively physicless games in Spring.",
+
+	modtype = "1",
+	depend = {
+		"cursors.sdz",
+	}
+}
+
+return modinfo
